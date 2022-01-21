0ed59728e85797e7da376a6002c365af06c264a7
Narek Tovmasyan <96180347+NarekTovmasyan@users.noreply.github.com>
2022-01-21 16:28:26 +0400

    add Tigran files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import { renderTablePage } from "./js/views/table"
 import { renderGeneralMenuPage } from "./js/views/generalMenu"
 import { renderDetailedPage } from "./js/views/detailed"
 import { renderFilterPage } from "./js/views/filter";
+
+
 import router from "./js/routing";
 
 //router.redirect("/menu");
+renderDetailedPage();
 renderFilterPage();
