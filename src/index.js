ce4089eae586ec61ae6fa631d1037565c7bcd4d7
Narek Tovmasyan <96180347+NarekTovmasyan@users.noreply.github.com>
2022-01-21 21:58:45 +0400

    just

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { renderTablePage } from "./js/views/table"
+import { renderTablePage } from "./js/views/table";
+import { renderGeneralMenuPage } from "./js/views/generalMenu";
 import router from "./js/routing";
 
 renderTablePage();
+
+
+let inputButton = document.querySelector(".connectToTable")
+inputButton.addEventListener("click", renderGeneralMenuPage);
